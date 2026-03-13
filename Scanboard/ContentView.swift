3e8b508885c3027e3646f5bfd7d48f1d9ca02b8a
katagaki <68231035+katagaki@users.noreply.github.com>
2026-03-13 22:41:31 +0900

    Initial Commit

---
+++ Scanboard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Scanboard
+//
+//  Created by シン・ジャスティン on 2026/03/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Scanboard/ScanboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScanboardApp.swift
+//  Scanboard
+//
+//  Created by シン・ジャスティン on 2026/03/13.
+//
+
+import SwiftUI
+
+@main
+struct ScanboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
